de0bb2775c842714d3d882747e3dfaf735401a0f
SvirepyiBacklazh <artur_iv03@mail.ru>
2023-12-02 22:46:17 +0500

    task_7

--- js/util.js
+++ js/util.js
@@ -3,6 +3,6 @@ export const getRandomInteger = (a, b) => {
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-  };
+};
 
-  export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
